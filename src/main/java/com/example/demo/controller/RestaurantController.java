e57c95642e684d805eb119e370c80a1ff635b9a4
barathkandasamy55-lab <barathkandasamy55@gmail.com>
2026-02-01 08:25:39 +0000

    code updated

--- src/main/java/com/example/demo/controller/RestaurantController.java
+++ src/main/java/com/example/demo/controller/RestaurantController.java
@@ -27,9 +27,16 @@ public Map<String, Map<String, Integer>> fetchRestaurants() {
     menu2.put("Paneer Makhani", 300);
     menu2.put("Roti", 20);
 
+    HashMap<String, Integer> menu3= new HashMap<String, Integer>();
+    menu3.put("Cold Coffee", 150);
+    menu3.put("Hot Coffee", 100);
+    mehu3.put("Sandwich", 100);
+
     Map<String, Map<String, Integer>> restaurants = new HashMap<>();
     restaurants.put("McDonalds", menu1);
     restaurants.put("IndianVeggieDelight", menu2);
+    restaurants.put("Johnscafe", menu3);
+
 
     return restaurants;
 }
