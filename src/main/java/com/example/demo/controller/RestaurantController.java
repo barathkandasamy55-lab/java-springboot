9876f038416ed366a47d85d05d8b77d79c3c89b1
barathkandasamy55-lab <barathkandasamy55@gmail.com>
2026-02-01 05:44:44 +0000

    first commit

---
+++ src/main/java/com/example/demo/controller/RestaurantController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+public class RestaurantController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome All";
+    }
+
+@GetMapping("/restaurants")
+public Map<String, Map<String, Integer>> fetchRestaurants() {
+
+    Map<String, Integer> menu1 = new HashMap<>();
+    menu1.put("Burger", 100);
+    menu1.put("Fries", 50);
+    menu1.put("Meal", 200);
+
+    Map<String, Integer> menu2 = new HashMap<>();
+    menu2.put("Dal Makhani", 200);
+    menu2.put("Paneer Makhani", 300);
+    menu2.put("Roti", 20);
+
+    Map<String, Map<String, Integer>> restaurants = new HashMap<>();
+    restaurants.put("McDonalds", menu1);
+    restaurants.put("IndianVeggieDelight", menu2);
+
+    return restaurants;
+}
+}
